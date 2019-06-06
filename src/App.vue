b87ad6a794f8a50fc0edadc1a4274416130bb518
arezki1 <a.ibsaine@hotmail.fr>
2019-06-06 10:40:20 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+
+    <HelloWorld msg="First Component"/>
+    <First msg="First Component"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+import First from "./components/First.vue";
+export default {
+  name: "app",
+  components: {
+    HelloWorld,
+    First
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/First.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="demo">
+    <h1>{{ msg }}</h1>
+    <p>hello</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "First",
+  props: {
+    msg: String
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.demo {
+  background-color: cyan;
+}
+</style>
+
+
